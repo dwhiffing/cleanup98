92643a52fdc83c2b53a20b6e8766f3d5caf2688b
Dan Whiffing <danielwhiffing@gmail.com>
2020-08-08 02:11:26 -0400

    Fixes task bar

--- src/components/TaskBar.js
+++ src/components/TaskBar.js
@@ -1,25 +1,36 @@
 import React from 'react'
 import windowsPng from '../assets/windows-4.png'
 
-export const TaskBar = ({ windows, onClickWindowItem }) => (
-  <div
-    className="window"
-    style={{
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      right: 0,
-      display: 'flex',
-    }}
-  >
-    <button className="start-button">
-      <img alt="Start" src={windowsPng} />
-      Start
-    </button>
-    {windows.map((w) => (
-      <button key={`task-bar-${w.index}`} onClick={() => onClickWindowItem(w)}>
-        {w.title}
+export const TaskBar = ({ windows, onClickWindowItem }) => {
+  return (
+    <div
+      className="window"
+      style={{
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        display: 'flex',
+      }}
+    >
+      <button className="start-button">
+        <img alt="Start" src={windowsPng} />
+        Start
       </button>
-    ))}
-  </div>
-)
+      {windows
+        .concat()
+        .sort((a, b) => a.index - b.index)
+        .map((w) => (
+          <button
+            key={`task-bar-${w.index}`}
+            className={
+              w.index === windows[windows.length - 1].index ? 'active' : ''
+            }
+            onClick={() => onClickWindowItem(w)}
+          >
+            {w.title}
+          </button>
+        ))}
+    </div>
+  )
+}
