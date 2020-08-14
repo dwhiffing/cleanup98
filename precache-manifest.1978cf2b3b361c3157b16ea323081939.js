28098bb984e429ab0570a30f6da310d96ff44241
Daniel Whiffing <danielwhiffing@gmail.com>
2020-08-14 12:24:36 -0400

    Updates

---
+++ precache-manifest.1978cf2b3b361c3157b16ea323081939.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557d4c49e2bd6fb0ac5683927721739a",
+    "url": "/cleanup98/index.html"
+  },
+  {
+    "revision": "1e74915a3a3bd53a6764",
+    "url": "/cleanup98/static/css/2.857d7983.chunk.css"
+  },
+  {
+    "revision": "f79d96cd7e42365b28be",
+    "url": "/cleanup98/static/css/main.6b0a8030.chunk.css"
+  },
+  {
+    "revision": "1e74915a3a3bd53a6764",
+    "url": "/cleanup98/static/js/2.fa022428.chunk.js"
+  },
+  {
+    "revision": "52f0de2c843087ab3fb5f96b8f26367f",
+    "url": "/cleanup98/static/js/2.fa022428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bcc36aebff9813f4774",
+    "url": "/cleanup98/static/js/3.ccedf14b.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/cleanup98/static/js/3.ccedf14b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79d96cd7e42365b28be",
+    "url": "/cleanup98/static/js/main.87d8abae.chunk.js"
+  },
+  {
+    "revision": "44c6c770c96ebc5de4c3",
+    "url": "/cleanup98/static/js/runtime-main.14556c02.js"
+  },
+  {
+    "revision": "19d21185d7a1d7e01c2feb3695862ba7",
+    "url": "/cleanup98/static/media/chord.19d21185.mp3"
+  },
+  {
+    "revision": "ae7aa376f670f697a7b3eddf17ac5227",
+    "url": "/cleanup98/static/media/click.ae7aa376.mp3"
+  },
+  {
+    "revision": "a8b420d4eb4e20d931ad11b4fd5dabac",
+    "url": "/cleanup98/static/media/ding.a8b420d4.mp3"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/cleanup98/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "7bbbd32e0f9977dd70a4d1287927d5da",
+    "url": "/cleanup98/static/media/ms_sans_serif.7bbbd32e.woff"
+  },
+  {
+    "revision": "6eb2c63fbead0014ef0c4c537bfe328a",
+    "url": "/cleanup98/static/media/ms_sans_serif_bold.6eb2c63f.woff"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/cleanup98/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  },
+  {
+    "revision": "601411cc922ce839d124b1e7b524ef24",
+    "url": "/cleanup98/static/media/recycle.601411cc.mp3"
+  },
+  {
+    "revision": "2e2ad3969c00ea92fdf9fb560378bf2d",
+    "url": "/cleanup98/static/media/startup.2e2ad396.mp3"
+  }
+]);
